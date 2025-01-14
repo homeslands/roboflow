98a93899bd7ff69f1990e411aab7c35fce530170
tuanvumaihuynh <tuanvux01@gmail.com>
2025-01-14 14:19:48 +0700

    fix: wrap div to remove scrollbar

--- ui/src/views/workflow/NewWorkflowBuilder.vue
+++ ui/src/views/workflow/NewWorkflowBuilder.vue
@@ -28,44 +28,46 @@ const edges = ref<Edge[]>([])
 </script>
 
 <template>
-  <div class="flex items-center justify-between">
-    <Breadcrumb>
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink>
-            <RouterLink to="/workflows" class="flex items-center text-xs">
-              Workflows
-            </RouterLink>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <Input
-            type="text"
-            placeholder="Enter name"
-            :model-value="workflowName"
-          />
-        </BreadcrumbItem>
-      </BreadcrumbList>
-    </Breadcrumb>
+  <div>
+    <div class="flex items-center justify-between">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink>
+              <RouterLink to="/workflows" class="flex items-center text-xs">
+                Workflows
+              </RouterLink>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <Input
+              type="text"
+              placeholder="Enter name"
+              :model-value="workflowName"
+            />
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
 
-    <div class="flex items-center space-x-2">
-      <Button variant="outline">
-        <XIcon />
-        Reset
-      </Button>
-      <Button>
-        Save
-      </Button>
+      <div class="flex items-center space-x-2">
+        <Button variant="outline">
+          <XIcon />
+          Reset
+        </Button>
+        <Button>
+          Save
+        </Button>
+      </div>
     </div>
-  </div>
 
-  <div class="relative flex h-full mt-3">
-    <WorkflowContainer>
-      <Workflow
-        :nodes="nodes"
-        :edges="edges"
-      />
-    </WorkflowContainer>
+    <div class="relative flex h-full mt-3">
+      <WorkflowContainer>
+        <Workflow
+          :nodes="nodes"
+          :edges="edges"
+        />
+      </WorkflowContainer>
+    </div>
   </div>
 </template>
